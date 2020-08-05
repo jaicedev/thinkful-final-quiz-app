f12eb1016efd95c455d253b76512c0e0d86dd155
Jaice de Celis <jaicedev@gmail.com>
2020-08-04 21:16:45 -0400

    Create store.js

---
+++ store.js
@@ -0,0 +1,107 @@
+// Hardcoding Questions using Object Literals
+const STORE = {
+    questions : [
+        {
+        question : "What is one basis point equivalent to in terms of pricing structure?",
+        options : [
+            "1%",
+            "0.1%",
+            "0.01%",
+            "0.001%"
+        ],
+        correctAnswer : "0.1%"
+        },
+        {
+        question : "What is PCI-Compliance?",
+        options : [
+            "Terminal Software",
+            "Data and Network Safety Criteria for Merchants",
+            "Post Credit Inquiry Compliance",
+            "Personal Computer Interface Compliance"
+        ],
+        correctAnswer : "Data and Network Safety Criteria for Merchants"
+        },
+        {
+        question : "What is MID short for?",
+        options : [
+            "Money Initialization Day",
+            "Merchant Identification",
+            "Merchant Infrastructure Duties",
+            "Max Increment Data"
+        ],
+        correctAnswer : "Merchant Identification"
+        },
+        {
+        question : "What pricing model passes all fees onto the customer?",
+        options : [
+            "Cash Discounting",
+            "Surcharging",
+            "Interchange Plus",
+            "Flat Rate"
+        ],
+        correctAnswer : "Cash Discounting"
+        },
+        {
+        question : "What pricing model passes some fees onto the customer?",
+        options : [
+            "Cash Discounting",
+            "Interchange Plus",
+            "Flat Rate",
+            "Surcharging"
+        ],
+        correctAnswer : "Surcharging"
+        },
+        {
+        question : "What pricing model is most common?",
+        options : [
+            "Surcharging",
+            "Flat Rate",
+            "Cash Discounting",
+            "Interchange Plus"
+        ],
+        correctAnswer : "Interchange Plus"
+        },
+        {
+        question : "If you have a month volume of $10000 and a profit margin of 15BPs how much are you making?",
+        options : [
+            "$1500",
+            "$150",
+            "$15",
+            "$1.50"
+        ],
+        correctAnswer : "$15"
+        },
+        {
+        question : "What is it called when a merchant stops processing?",
+        options : [
+            "Drop out",
+            "Deconversion",
+            "Cancellation",
+            "Merchant Request"
+        ],
+        correctAnswer : "Deconversion"
+        },
+        {
+        question : "What is the most common residual split in the industry?",
+        options : [
+            "20%",
+            "30%",
+            "40%",
+            "50%"
+        ],
+        correctAnswer : "40%"
+        },
+        {
+        question : "How many new merchants does the average MS Sales Person Sign Monthly?",
+        options : [
+            "3",
+            "5",
+            "9",
+            "11"
+        ],
+        correctAnswer : "9"
+        },
+    ],
+    currentQuestion: 0,
+    currentScore: 0
+}
